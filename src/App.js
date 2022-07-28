f650fdf817de14ca63dbe46e004b6bc5dfaa3356
Wayd26 <wayedoun@gmail.com>
2022-07-28 03:01:02 +0100

    implement hero banner

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ import ExerciseDetails from './pages/ExerciseDetails';
 import Footer from './components/Footer';
 
 const App = () => {
+
   return (
-    <Box width="400px" >
+    <Box width="400px" sx={{width: {xl: '1488px'}}} m="auto">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
